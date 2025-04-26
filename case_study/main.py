5055fb760d12ab3b66b75b7499cead24cd843c26
Melissa <mrlmarquez@users.noreply.github.com>
2025-04-27 00:55:03 +0800

    my own module couldnt be found lol

--- case_study/main.py
+++ case_study/main.py
@@ -1,3 +1,10 @@
+import os
+
+import sys
+
+os.chdir("/mount/src/case_study/src")
+sys.path.append("/mount/src/case_study/src/")
+
 from case_study.rag.langchain.loader import load_contracts, load_rules
 from case_study.ui.app import start
 
