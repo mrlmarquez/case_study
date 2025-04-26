444d458cbb5e42be151cc5fffdb25b78ccb22a72
Melissa <mrlmarquez@users.noreply.github.com>
2025-04-27 00:59:09 +0800

    sql version debugging

--- case_study/main.py
+++ case_study/main.py
@@ -5,6 +5,10 @@ import sys
 os.chdir("/mount/src/case_study/")
 sys.path.append("/mount/src/case_study/")
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from case_study.rag.langchain.loader import load_contracts, load_rules
 from case_study.ui.app import start
 
